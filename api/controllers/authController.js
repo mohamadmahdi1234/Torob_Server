8b02b51f0d0e2dec117edda1234bc1adc5f0cb63
mahdy <mohamadnoori123987@gmail.com>
2022-06-27 21:32:53 +0430

    little improve in authcontroller.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,8 +2,29 @@ const mongoose = require("mongoose");
 const Otp = require('../models/OTP');
 const User = require('../models/User');
 const bcrypt = require("bcrypt");
+const error_400_bad_request = require('../Error_400');
 
 const userSignup = async (req,res)=>{
+    try{
+        const Users = await User.find({name:req.body.name}).exec();
+        if(Users.length<1){
+            const hash_pass = await bcrypt.hash(req.body.password, 10);
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash_pass,
+                name : req.body.name,
+              });
+
+
+        }else{
+            return error_400_bad_request(res,'user already exist!');
+        }
+
+    }catch(err){
+        console.log(err);
+        return error_400_bad_request(res,'bad request!');
+    }
 
 };
 
