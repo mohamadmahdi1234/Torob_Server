de334cb4c6d5468233f45c85a2b87b1c14629aa9
mahdy <mohamadnoori123987@gmail.com>
2022-06-28 00:03:27 +0430

    tokenGenerator.js added to directory of project

---
+++ api/tokenGenerator.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+ const Generator = (user_email,user_id)=>{
+    return jwt.sign(
+        {
+          email: user_email,
+          userId: user_id
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "1m"
+        }
+      );
+
+ }
+
+ module.exports = {Generator};
