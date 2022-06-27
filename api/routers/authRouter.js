54a3d842eb80bbc1ae4c81948bdfa08166ed7d95
mahdy <mohamadnoori123987@gmail.com>
2022-06-27 18:08:55 +0430

    edit in authRouter.js direction for authcontroller.js

--- api/routers/authRouter.js
+++ api/routers/authRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {user_signup,user_login} = require('../controllers/auth_stuff/auth_stuff_controllers');
+const {user_signup,user_login} = require('../controllers/authController');
 router.post('/signup',user_signup);
 router.post('/login',user_login);
 module.exports = router;
