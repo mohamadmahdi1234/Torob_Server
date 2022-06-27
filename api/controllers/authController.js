adbbd87405bfb94e7cc215b3e748829cd1b63208
mahdy <mohamadnoori123987@gmail.com>
2022-06-27 21:20:21 +0430

    little code for signup in authcontroller.js

---
+++ api/controllers/authController.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Otp = require('../models/OTP');
+const User = require('../models/User');
+const bcrypt = require("bcrypt");
+
+const userSignup = async (req,res)=>{
+
+};
+
+module.exports= {userSignup};
