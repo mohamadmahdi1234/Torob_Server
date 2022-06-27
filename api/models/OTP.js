9ad876fdbc09e3c9659bd381fedde652d223d654
mahdy <mohamadnoori123987@gmail.com>
2022-06-27 18:52:02 +0430

    add otp model with initialize code

---
+++ api/models/OTP.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const OTPSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    number : {type:String,required: true},
+    otp:{
+        type:String,
+        required:true
+    },
+    createdAt:{type:Date,default:Date.now,index:{expires:300}}
+    
+},{timestamps:true});
+
+module.exports = mongoose.model('OTP', OTPSchema);
