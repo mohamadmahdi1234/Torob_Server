6e8d544c02b5a4853ff13e51b5e1aedc792d8ea5
mahdy <mohamadnoori123987@gmail.com>
2022-06-27 23:49:22 +0430

    complete signup in authcontroller.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,6 +3,14 @@ const Otp = require('../models/OTP');
 const User = require('../models/User');
 const bcrypt = require("bcrypt");
 const error_400_bad_request = require('../Error_400');
+const otpGenerator = require('otp-generator');
+const OTP = require("../models/OTP");
+const nodemailer = require("nodemailer");
+const {google} = require('googleapis');
+require('dotenv').config();
+const OAuth2 = google.auth.OAuth2;
+const OAuth2_client = new OAuth2(process.env.CLIENT_ID,process.env.CLIENT_PASS);
+OAuth2_client.setCredentials({refresh_token:process.env.REFRESH_TOKEN});
 
 const userSignup = async (req,res)=>{
     try{
@@ -16,12 +24,44 @@ const userSignup = async (req,res)=>{
                 password:req.body.password ,
                 name : req.body.name,
               });
-              await user.save();
-              return res.status(200).json({
-                message:"yes"
-              });
+            const otp_value = otpGenerator.generate(6,{
+                digits:true,lowerCaseAlphabets:false,upperCaseAlphabets:false,specialChars:false,
+            });
+            console.log(otp_value);
+            const otp_value_hash = await bcrypt.hash(otp_value, 10); 
+            const otp_record = new OTP({
+                _id: new mongoose.Types.ObjectId(),
+                user_email:req.body.email,
+                otp:otp_value_hash,
+                email: req.body.email,
+                password:req.body.password ,
+                name : req.body.name,
+            });
+            await otp_record.save();
+            const access_token = OAuth2_client.getAccessToken();
+            const transport = nodemailer.createTransport({
+                service:'gmail',
+                auth:{
+                    type:'OAuth2',
+                    user:'mohamadnoori123@gmail.com',
+                    clientId:process.env.CLIENT_ID,
+                    clientSecret:process.env.CLIENT_PASS,
+                    refreshToken:process.env.REFRESH_TOKEN,
+                    accessToken:access_token
 
-
+                },
+            });
+            
+            await transport.sendMail({
+                from:`OTP CODE <mohamadnoori123@gmail.com>`,
+                to:req.body.email,
+                subject:"CONFIRM CODE",
+                text:`hello pls input this code before five min ${otp_value}`
+            });    
+            return res.status(200).json({
+                message:"email sent!",
+            });
+              
         }else{
             return error_400_bad_request(res,'user already exist!');
         }
@@ -33,4 +73,15 @@ const userSignup = async (req,res)=>{
 
 };
 
-module.exports= {userSignup};
+const confirmOTP = async(req,res)=>{
+    try{
+
+
+    }catch(err){
+        console.log(err);
+        return error_400_bad_request(res,err.message);
+    }
+
+};
+
+module.exports= {userSignup,confirmOTP};
