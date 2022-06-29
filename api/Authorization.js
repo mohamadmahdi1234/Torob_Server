e52cf032c69bd1ef9a19c4c2abdd019f36a264c7
mahdy <mohamadnoori123987@gmail.com>
2022-06-29 18:43:48 +0430

    authorization  module completed

---
+++ api/Authorization.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const error_400_bad = require('./Error_400');
+//this part is from youtube maximilian
+const Authorization = (req, res, next) => {
+    try {
+        const decoded = jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error) {
+        console.log(error);
+        return error_400_bad(res,'pls sign in!');
+    }
+};
+
+module.exports = Authorization;
