661a189e5a3c8cf5f1ca8ba355629fee6a206028
mahdy <mohamadnoori123987@gmail.com>
2022-06-29 10:33:41 +0430

    add router for signin

--- api/routers/authRouter.js
+++ api/routers/authRouter.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const {userSignup,confirmOTP} = require('../controllers/authController');
 router.post('/signup',userSignup);
 router.post('/verifyOTP',confirmOTP);
+router.post('/signin',confirmOTP);
 module.exports = router;
